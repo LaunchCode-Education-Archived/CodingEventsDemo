78cdef75a80755f150d96d94978afe10dcedda8c
Carly Langlois <cj.langlois.cl@gmail.com>
2020-04-21 10:41:05 -0500

    views exercises starter code

---
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace coding_events_practice.Controllers
+{
+    public class EventsController : Controller
+    {
+
+        static private List<string> Events = new List<string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+
+            return Redirect("/Events");
+        }
+    }
+}
