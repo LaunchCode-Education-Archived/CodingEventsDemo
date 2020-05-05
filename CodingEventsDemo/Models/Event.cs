b17bb26196af096806e38c6e3902bd61d987d0e2
Carly Langlois <cj.langlois.cl@gmail.com>
2020-05-05 14:29:30 -0500

    create event model class

---
+++ CodingEventsDemo/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CodingEventsDemo.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Event()
+        {
+        }
+    }
+}
