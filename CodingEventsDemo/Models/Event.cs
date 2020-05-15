56b9ee6ac485f866b74ef266b63046801f8b898f
Carly Langlois <cj.langlois.cl@gmail.com>
2020-05-15 12:28:08 -0500

    add event id to class and table view

--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -6,15 +6,31 @@ namespace CodingEventsDemo.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public int Id { get; }
+        static private int nextId = 1;
+
         public Event(string name, string description)
         {
             Name = name;
             Description = description;
+            Id = nextId;
+            nextId++;
         }
 
         public override string ToString()
         {
             return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                   Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
         }
     }
 }
