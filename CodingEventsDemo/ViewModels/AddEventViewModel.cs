127f45904b1195a2d6b1e6bc8f80613ada176090
Carly Langlois <cj.langlois.cl@gmail.com>
2020-06-20 17:43:52 -0500

    mod error messages

--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -6,11 +6,11 @@ namespace CodingEventsDemo.ViewModels
     public class AddEventViewModel
     {
         [Required(ErrorMessage = "Name is required.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Description is required")]
-        [StringLength(500, ErrorMessage = "Description too long!")]
+        [Required(ErrorMessage = "Please enter a description for your event.")]
+        [StringLength(500, ErrorMessage = "Description is too long!")]
         public string Description { get; set; }
 
         [EmailAddress]
