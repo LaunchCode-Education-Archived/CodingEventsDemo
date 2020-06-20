c17c7be9b406120df92d0ad00a7133a0cc589bb2
Carly Langlois <cj.langlois.cl@gmail.com>
2020-06-20 17:39:01 -0500

    remove duplicate names and commented delete events title

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -46,7 +46,6 @@ namespace coding_events_practice.Controllers
 
         public IActionResult Delete()
         {
-            //ViewBag.title = "Delete Events";
             ViewBag.events = EventData.GetAll();
 
             return View();
