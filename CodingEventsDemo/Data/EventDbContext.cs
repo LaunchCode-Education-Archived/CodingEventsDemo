c984c61efce6b99427db2e93140055987f64d391
Sally Steuterman <sally.steuterman@gmail.com>
2020-06-25 15:57:44 -0500

    Adding solution to exercises

--- CodingEventsDemo/Data/EventDbContext.cs
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -6,6 +6,7 @@ namespace CodingEventsDemo.Data
     public class EventDbContext : DbContext
     {
         public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> Categories { get; set; }
 
         public EventDbContext(DbContextOptions<EventDbContext> options)
             : base(options)
--- CodingEventsDemo/Migrations/20200625205525_CategoryMigration.Designer.cs
+++ CodingEventsDemo/Migrations/20200625205525_CategoryMigration.Designer.cs
@@ -2,14 +2,16 @@
 using CodingEventsDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodingEventsDemo.Migrations
 {
     [DbContext(typeof(EventDbContext))]
-    partial class EventDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200625205525_CategoryMigration")]
+    partial class CategoryMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
