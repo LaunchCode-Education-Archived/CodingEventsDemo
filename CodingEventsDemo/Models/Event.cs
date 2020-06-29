fa3391eddee12f2fdd319d92e599b195fc811d1f
Carly Langlois <cj.langlois.cl@gmail.com>
2020-06-29 09:48:10 -0500

    post method name, constructor chaining

--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -16,20 +16,20 @@ namespace CodingEventsDemo.Models
         public int Id { get; }
         static private int nextId = 1;
 
-        public Event(string name, string description, string contactEmail)
-        {
-            Name = name;
-            Description = description;
-            ContactEmail = contactEmail;
-            Id = nextId;
-            nextId++;
-        }
-
         public Event()
         {
             Id = nextId;
             nextId++;
         }
+
+        public Event(string name, string description, string contactEmail) : this()
+        {
+            Name = name;
+            Description = description;
+            ContactEmail = contactEmail;
+        }
+
+        
 
         public override string ToString()
         {
