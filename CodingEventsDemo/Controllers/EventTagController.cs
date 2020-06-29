c8408ba63a3ee5aadd2e47d1abc28dd9dc37198b
Chris Bay <chris@chrisbay.net>
2020-06-29 15:24:01 -0500

    Adds EventTag model w/o relationships

---
+++ CodingEventsDemo/Controllers/EventTagController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventTagController : Controller
+    {
+        private EventDbContext context;
+
+        public EventTagController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<EventTag> tags = context.Tags.ToList();
+            return View(tags);
+        }
+
+        public IActionResult Add()
+        {
+            EventTag tag = new EventTag();
+            return View(tag);
+        }
+
+        [HttpPost]
+        public IActionResult Add(EventTag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Tags.Add(tag);
+                context.SaveChanges();
+                return Redirect("/EventTag/");
+            }
+
+            return View("Add", tag);
+        }
+    }
+}
---
+++ CodingEventsDemo/Models/EventTag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEventsDemo.Models
+{
+    public class EventTag
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        public EventTag(string name)
+        {
+            Name = name;
+        }
+
+        public EventTag()
+        {
+        }
+    }
+}
