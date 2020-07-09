02e0d981e9b3175b4c80a887bb3cbc019f004d10
Chris Bay <chris@chrisbay.net>
2020-07-09 07:02:37 -0500

    Creates /Tag/AddEvent/X view/path

--- CodingEventsDemo/Controllers/TagController.cs
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -46,5 +46,38 @@ namespace CodingEventsDemo.Controllers
 
             return View("Add", tag);
         }
+
+        public IActionResult AddEvent(int id)
+        {
+            Event theEvent = context.Events.Find(id);
+            List<Tag> possibleTags = context.Tags.ToList();
+
+            AddEventTagViewModel viewModel = new AddEventTagViewModel(theEvent, possibleTags);
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventTagViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                int eventId = viewModel.EventId;
+                int tagId = viewModel.TagId;
+
+                EventTag eventTag = new EventTag
+                {
+                    EventId = eventId,
+                    TagId = tagId
+                };
+
+                context.EventTags.Add(eventTag);
+                context.SaveChanges();
+
+                return Redirect("/Events/Detail/" + eventId);
+            }
+
+            return View(viewModel);
+        }
     }
 }
---
+++ CodingEventsDemo/ViewModels/AddEventTagViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventTagViewModel
+    {
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public List<SelectListItem> Tags { get; set; }
+
+        public int TagId { get; set; }
+
+        public AddEventTagViewModel(Event theEvent, List<Tag> possibleTags)
+        {
+            Tags = new List<SelectListItem>();
+
+            foreach (var tag in possibleTags)
+            {
+                Tags.Add(new SelectListItem {
+                    Value = tag.Id.ToString(),
+                    Text = tag.Name
+                });
+            }
+
+            Event = theEvent;
+        }
+
+        public AddEventTagViewModel()
+        {
+        }
+    }
+}
