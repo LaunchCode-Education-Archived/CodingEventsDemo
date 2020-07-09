de58ed09973224eab684510690c7c03ad4d36397
Chris Bay <chris@chrisbay.net>
2020-07-09 06:38:58 -0500

    Creates EventTag join class

---
+++ CodingEventsDemo/Migrations/20200709113546_AddsEventTag.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodingEventsDemo.Migrations
+{
+    public partial class AddsEventTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventTags",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(nullable: false),
+                    TagId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventTags", x => new { x.EventId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_EventTags_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventTags_TagId",
+                table: "EventTags",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventTags");
+        }
+    }
+}
---
+++ CodingEventsDemo/Models/EventTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CodingEventsDemo.Models
+{
+    public class EventTag
+    {
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+
+        public EventTag()
+        {
+        }
+    }
+}
