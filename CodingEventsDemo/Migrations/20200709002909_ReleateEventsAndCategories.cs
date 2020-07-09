273616482705bf23682333d0e67a55344a2dd55a
Chris Bay <chris@chrisbay.net>
2020-07-08 19:40:05 -0500

    Creates db migration

---
+++ CodingEventsDemo/Migrations/20200709002909_ReleateEventsAndCategories.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodingEventsDemo.Migrations
+{
+    public partial class ReleateEventsAndCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Events",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
