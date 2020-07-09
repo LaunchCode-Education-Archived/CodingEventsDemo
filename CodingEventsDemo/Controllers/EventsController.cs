3e85075149ec4fe0e1fc9b95a18947222eb2fbbe
Chris Bay <chris@chrisbay.net>
2020-07-09 07:37:07 -0500

    Adds tags to event detail view

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -90,7 +90,12 @@ namespace coding_events_practice.Controllers
                .Include(e => e.Category)
                .Single(e => e.Id == id);
 
-            EventDetailViewModel viewModel = new EventDetailViewModel(theEvent);
+            List<EventTag> eventTags = context.EventTags
+                .Where(et => et.EventId == id)
+                .Include(et => et.Tag)
+                .ToList();
+
+            EventDetailViewModel viewModel = new EventDetailViewModel(theEvent, eventTags);
             return View(viewModel);
         }
     }
