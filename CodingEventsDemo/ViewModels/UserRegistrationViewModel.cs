303c8860fb02308c071ed9772591a4673fb4adb2
Sally Steuterman <sally.steuterman@gmail.com>
2020-07-15 11:16:02 -0500

    Initial registration logic

---
+++ CodingEventsDemo/ViewModels/UserRegistrationViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class UserRegistrationViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
