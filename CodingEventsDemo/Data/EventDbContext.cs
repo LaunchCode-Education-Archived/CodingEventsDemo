85db4aaf39a4e420b85bbf83b12bac5de190b0e6
saranell <saranell92@gmail.com>
2022-11-22 17:55:53 -0600

    6.2 EventTag model

--- CodingEventsDemo/Data/EventDbContext.cs
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -8,10 +8,16 @@ namespace CodingEventsDemo.Data
         public DbSet<Event> Events { get; set; }
         public DbSet<EventCategory> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<EventTag> EventTags { get; set; }
 
         public EventDbContext(DbContextOptions<EventDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EventTag>().HasKey(et => new { et.EventId, et.TagId });
         }
     }
 }
