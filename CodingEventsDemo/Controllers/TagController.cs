f2f45d209f0638a10e42f01d693edaa00d7745bc
saranell <saranell92@gmail.com>
2022-11-23 14:52:20 -0600

    6.3 adding a tag to an event

--- CodingEventsDemo/Controllers/TagController.cs
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Controllers
@@ -40,6 +41,39 @@ namespace CodingEventsDemo.Controllers
             }
 
             return View("Add", tag);
+        }
+
+        public IActionResult AddEvent(int id)
+        {
+            Event theEvent = context.Events.Find(id);
+            List<Tag> possibleTags = context.Tags.ToList();
+
+            AddEventTagViewModel viewModel = new AddEventTagViewModel(theEvent, possibleTags);
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventTagViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                int eventId = viewModel.EventId;
+                int tagId = viewModel.TagId;
+
+                EventTag eventTag = new EventTag
+                {
+                    EventId = eventId,
+                    TagId = tagId
+                };
+
+                context.EventTags.Add(eventTag);
+                context.SaveChanges();
+
+                return Redirect("/Events/Detail/" + eventId);
+            }
+
+            return View(viewModel);
         }
     }
 }
